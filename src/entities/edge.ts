3909b0842508643414ba77eca4931cc8bf9405d1
tomast1337 <vycasnicolas@gmail.com>
2023-06-17 04:40:10 -0300

    Added color atribute to edge

--- src/entities/edge.ts
+++ src/entities/edge.ts
@@ -4,12 +4,5 @@ export interface Edge<T> {
   node1: GNode<T>;
   node2: GNode<T>;
   weight: number;
+  color?: string;
 }
-
-export const example = () => {
-  const edge: Edge<string> = {
-    node1: new GNode("A"),
-    node2: new GNode("B"),
-    weight: 10,
-  };
-};
